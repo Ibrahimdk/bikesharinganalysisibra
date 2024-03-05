57aed3d25afee27de611d0750c8f2e8aa3ce9d7a
Ibrahimdk <92035432+Ibrahimdk@users.noreply.github.com>
2024-03-05 01:47:24 +0000

    commit

--- Hello.py
+++ Hello.py
@@ -1,51 +1,108 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import streamlit as st
-from streamlit.logger import get_logger
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.cluster import KMeans
 
-LOGGER = get_logger(__name__)
+# Load dataset
+@st.cache
+def load_data():
+    df = pd.read_csv("day.csv")
+    return df
 
+# Main function
+def main():
+    st.title("Bike Sharing Clustering & Visualitation Dashboard ")
 
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
+    # Load data
+    df = load_data()
 
-    st.write("# Welcome to Streamlit! 👋")
+    # Pilih fitur yang ingin digunakan untuk clustering
+    selected_features = ['temp', 'hum', 'windspeed']
 
-    st.sidebar.success("Select a demo above.")
+    # Memilih jumlah cluster
+    num_clusters = st.slider("Jumlah Cluster:", 2, 10)
 
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
+    # Membuat objek KMeans
+    kmeans = KMeans(n_clusters=num_clusters, random_state=42)
 
+    # Melakukan clustering
+    clusters = kmeans.fit_predict(df[selected_features])
+
+    # Menambahkan kolom hasil clustering ke dataframe
+    df['cluster'] = clusters
+
+    # Menampilkan data yang telah di-cluster
+    st.subheader("Data setelah di-cluster:")
+    st.write(df)
+
+    # Menampilkan scatter plot berdasarkan dua fitur yang dipilih
+    x_axis = st.selectbox("Pilih Fitur untuk Sumbu X:", selected_features)
+    y_axis = st.selectbox("Pilih Fitur untuk Sumbu Y:", selected_features, index=1)
+    plt.figure(figsize=(10, 6))
+    sns.scatterplot(data=df, x=x_axis, y=y_axis, hue='cluster', palette='viridis', legend='full')
+    plt.title(f"Clustering berdasarkan {x_axis} vs {y_axis}")
+    plt.xlabel(x_axis)
+    plt.ylabel(y_axis)
+    st.pyplot()
+    
+    
+    st.title("Data Visualitation")
+    #Pertanyaan 1
+    st.subheader("Jumlah peminjaman sepeda berdasarkan bulan")
+    monthly_rentals = df.groupby('mnth')['cnt'].sum().reset_index()
+    plt.figure(figsize=(10, 6))
+    sns.barplot(x='mnth', y='cnt', data=monthly_rentals, palette='Blues_r')
+    plt.title('Jumlah Peminjaman Sepeda Berdasarkan Bulan')
+    plt.xlabel('Bulan')
+    plt.ylabel('Jumlah Peminjaman')
+    st.pyplot()
+    st.set_option('deprecation.showPyplotGlobalUse', False)
+
+    #Pertanyaan 2
+    st.subheader("Perbandingan jumlah peminjaman sepeda pada Working Day dan Other Day")
+    weekday_counts = df[df['workingday'] == 1]['cnt'].sum()
+    workingday_counts = df[df['workingday'] == 0]['cnt'].sum()
+
+    plt.figure(figsize=(8, 8))
+    plt.pie([weekday_counts, workingday_counts], labels=['Working Day', 'Other Day'], autopct='%1.1f%%', colors=['#A0E9FF', '#CDF5FD'])
+    plt.title('Perbandingan Jumlah Peminjaman Sepeda pada Working Day dan Other Day')
+    st.pyplot()
+
+    #Pertanyaan 3 
+    st.subheader("Jumlah peminjaman sepeda berdasarkan musim")
+    season_rentals = df.groupby('season')['cnt'].sum().reset_index()
+    custom_colors = ['#D5F0C1', '#FFCF81', '#FFBE98', '#AEE2FF']
+
+    plt.figure(figsize=(8, 6))
+    sns.barplot(x='season', y='cnt', data=season_rentals, palette=custom_colors)
+    plt.title('Jumlah Peminjaman Sepeda Berdasarkan Musim')
+    plt.xlabel('Musim')
+    plt.ylabel('Jumlah Peminjaman')
+    plt.xticks([0, 1, 2, 3], ['Spring', 'Summer', 'Fall', 'Winter'])
+    st.pyplot()
+
+     #Pertanyaan 4
+    st.subheader("Jumlah peminjaman sepeda berdasarkan cuaca dengan donut chart")
+    weather_rentals = df.groupby('weathersit')['cnt'].sum().reset_index()
+
+    plt.figure(figsize=(8, 8))
+    plt.pie(weather_rentals['cnt'], labels=weather_rentals['weathersit'], autopct='%1.1f%%', colors=['#FFBE98', '#F7DED0', '#FEECE2'], wedgeprops=dict(width=0.4))
+    centre_circle = plt.Circle((0,0),0.70,fc='white')
+    fig = plt.gcf()
+    fig.gca().add_artist(centre_circle)
+    plt.title('Jumlah Peminjaman Sepeda Berdasarkan Cuaca')
+    plt.axis('equal')
+    st.pyplot()
+
+    # Pertanyaan 5
+    st.subheader('Pengaruh Peminjaman Sepeda Berdasarkan Kecepatan Angin')
+    plt.figure(figsize=(10, 6))
+    sns.scatterplot(x='windspeed', y='cnt', data=df, color='orange')
+    plt.title('Peminjaman Sepeda Berdasarkan Kecepatan Angin')
+    plt.xlabel('Kecepatan Angin')
+    plt.ylabel('Jumlah Peminjaman')
+    st.pyplot()
 
 if __name__ == "__main__":
-    run()
+    main()
--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-# Welcome to streamlit
+conda create --name main-ds python=3.9
+conda activate main-ds
+pip install numpy pandas  matplotlib seaborn 
 
-This is the app you get when you run `streamlit hello`, extracted as its own app.
 
-Edit [Hello.py](./Hello.py) to customize this app to your heart's desire. ❤️
 
-Check it out on [Streamlit Community Cloud](https://st-hello-app.streamlit.app/)
+streamlit run dashboard.py
+
